b8293ac55d01ff47d5aece2ae1f6e2cd7c22c8d6
Wojciech Leksza <wojtek.leksza@gmail.com>
2022-09-12 10:42:47 +0200

    rendering task list by JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
 {
-    
+    let tasks = [
+        {
+            content: "pozmywać naczynia",
+            done: false,
+        },
+        {
+            content: "odrobić zadanie z YouCode",
+            done: true,
+        }
+    ];
+
+    const render = () => {
+        const taskListElement = document.querySelector(".js-taskList");
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += 
+                `<li class="section__listItem">
+                    <button class="section__doneButton"></button>
+                    <div class="section__taskTextContainer">${task.content}</div>
+                    <button class="section__removeButton">&#128465</button>
+                </li>`
+        }
+
+        taskListElement.innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
+    };
+
+    init();
 }
