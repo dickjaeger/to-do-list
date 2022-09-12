6ca61e975614309c147099cab04c9aab2f78d101
Wojciech Leksza <wojtek.leksza@gmail.com>
2022-09-12 11:11:55 +0200

    adding tasks by JS

--- js/main.js
+++ js/main.js
@@ -10,12 +10,34 @@
         }
     ];
 
+    const addTask = () => {
+        const newTaskContent = document.querySelector(".js-taskContent").value.trim();
+
+        if (!newTaskContent) {
+            return;
+        }
+
+        tasks.push({
+            content: newTaskContent,
+        })
+    }
+
+    const onFormSubmit = () => {
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            addTask();
+            render();
+        });
+    }
+
     const render = () => {
         const taskListElement = document.querySelector(".js-taskList");
         let htmlString = "";
 
         for (const task of tasks) {
-            htmlString += 
+            htmlString +=
                 `<li class="section__listItem">
                     <button class="section__doneButton"></button>
                     <div class="section__taskTextContainer">${task.content}</div>
@@ -28,6 +50,7 @@
 
     const init = () => {
         render();
+        onFormSubmit();
     };
 
     init();
