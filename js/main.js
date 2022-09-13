7da4bc720151beecb776883999ae31c389e6ce13
Wojciech Leksza <wojtek.leksza@gmail.com>
2022-09-13 13:52:55 +0200

    rendering buttons toggling button show/hide done tasks

--- js/main.js
+++ js/main.js
@@ -10,11 +10,13 @@
         },
     ];
 
+    let readyTaskHided = false;
+
     const addTask = (newTaskContent) => {
         if (newTaskContent === "") {
             return;
         };
-        
+
         tasks = [
             ...tasks,
             { content: newTaskContent },
@@ -26,7 +28,7 @@
     const removeTask = (taskIndex) => {
         tasks = [
             ...tasks.slice(0, taskIndex),
-            ...tasks.slice(taskIndex +1),
+            ...tasks.slice(taskIndex + 1),
         ];
 
         render();
@@ -36,10 +38,14 @@
         tasks = [
             ...tasks.slice(0, taskIndex),
             { ...tasks[taskIndex], done: !tasks[taskIndex].done },
-            ...tasks.slice(taskIndex +1),
+            ...tasks.slice(taskIndex + 1),
         ];
 
         render();
+    };
+
+    const toggleHideDoneTask = () => {
+        readyTaskHided = !readyTaskHided;
     };
 
     const onFormSubmit = (event) => {
@@ -49,7 +55,7 @@
         const newTaskContent = newTaskContentElement.value.trim();
 
         addTask(newTaskContent);
-        
+
         newTaskContentElement.value = "";
         newTaskContentElement.focus();
     };
@@ -68,9 +74,16 @@
                 removeTask(index);
             });
         });
+
+        const toggleHideDoneTaskButton = document.querySelector(".js-toggleHideDoneTask");
+        if (toggleHideDoneTaskButton) {
+            toggleHideDoneTaskButton.addEventListener("click", () => {
+                toggleHideDoneTask();
+            });
+        };
     };
 
-    const render = () => {
+    const renderList = () => {
         const taskListElement = document.querySelector(".js-taskList");
         let htmlString = "";
 
@@ -88,7 +101,24 @@
         };
 
         taskListElement.innerHTML = htmlString;
+    }
 
+    const renderButtons = () => {
+        const buttonsContainerElement = document.querySelector(".js-buttonsContainer");
+        const htmlString =
+            tasks.length === 0 ? "" :
+                `
+                <button class="section__button js-toggleHideDoneTask">
+                        ${readyTaskHided ? "Pokaż ukończone" : "Ukryj ukończone"}
+                </button>
+                <button class="section__button">Ukończ wszystkie</button>
+            `;
+        buttonsContainerElement.innerHTML = htmlString;
+    }
+
+    const render = () => {
+        renderList();
+        renderButtons();
         bindEvents();
     };
 
